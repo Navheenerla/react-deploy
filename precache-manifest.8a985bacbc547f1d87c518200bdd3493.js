47b5924282bb7461c6852e6c77009b107b0988d3
naveen <naveen.erla@trupik.com>
2020-06-10 13:34:48 +0530

    Updates

---
+++ precache-manifest.8a985bacbc547f1d87c518200bdd3493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e659b770551b00ac3100beb2efb0cb",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "7e7e4fb15dfbe35b1900",
+    "url": "/react-deploy/static/css/main.fc22a8a2.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7e4fb15dfbe35b1900",
+    "url": "/react-deploy/static/js/main.9341df90.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  },
+  {
+    "revision": "5ece580738b05198ab0188d8657292b6",
+    "url": "/react-deploy/static/media/yoda.5ece5807.jpg"
+  }
+]);
